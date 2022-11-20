a0c0369f8810964a848e51a334fb50f7106e83d9
unknown <rickywai77@gmail.com>
2022-11-20 23:47:04 +0800

    [Ricky] - cropping

--- dataProcessing.py
+++ dataProcessing.py
@@ -4,4 +4,10 @@ import rawpy
 import os
 import matplotlib.pyplot as plt 
 import math
+import imUtils
+
+folder = 'test_images/'
+ori_img = imUtils.imread(folder + '1.cr3')
+
+patchPos = imUtils.get4ColourPatchPos(ori_img.copy())
 
