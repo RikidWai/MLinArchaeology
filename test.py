e749862e7e3bb9f27dea1e4163b4d76242dae06f
Rikid <89850471+RikidWai@users.noreply.github.com>
2022-12-28 16:29:48 +0800

    [Ricky] - looping demo

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import os
-rootdir = 'C:/Users/sid/Desktop/test'
-
-for subdir, dirs, files in os.walk(rootdir):
+rootdir = '/content/drive/MyDrive/export_images'
+targetdir = '/content/drive/MyDrive/CS_FYP_archaeology/test_images/'
+# Loop over differ
+for root, dirs, files in os.walk(rootdir):
     for file in files:
-        print(os.path.join(subdir, file))
+        filename, extension = os.path.splitext(file)
+        if 'cr' in extension or 'CR' in extension:
+            path = os.path.join(root, file)
+            dir = root.split(os.path.sep)[-1]
+            print(f'{dir}_{filename}_s{1}')
