da8092ebfcd49fa29e8a05889664f2f0f2fb8221
unknown <rickywai77@gmail.com>
2023-01-01 21:47:02 +0800

    Update labelling.py

--- Labelling/labelling.py
+++ Labelling/labelling.py
@@ -1,5 +1,4 @@
 # Import libraries
-import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from pathlib import Path
@@ -21,19 +20,16 @@ def generateEncoding():
         le = LabelEncoder()
         df_labelEncoding.fabric = le.fit_transform(
             df_labelEncoding.fabric.astype(str))
-        print(df_labelEncoding)
         filepath = Path('../Labelling/labelEncoding.csv')
         df_labelEncoding.to_csv(filepath)
 
         le_name_mapping = pd.DataFrame(
             zip(le.classes_, le.transform(le.classes_)), columns=['Class', 'EncodedLabel'])
-        print(le_name_mapping)
         filepath = Path('../Labelling/labelEncodingMapping.csv')
         le_name_mapping.to_csv(filepath)
 
         # One-Hot Encoding
         one_hot_encoded_data = pd.get_dummies(df, columns=['fabric'])
-        print(one_hot_encoded_data)
         filepath = Path('../Labelling/oneHotEncoding.csv')
         one_hot_encoded_data.to_csv(filepath)
         
