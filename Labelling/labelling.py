f0c3876bf350011a5c6150a71a80feb009bccfe5
unknown <rickywai77@gmail.com>
2023-01-01 21:45:01 +0800

    adding generateEncoding before processing data

---
+++ Labelling/labelling.py
@@ -0,0 +1,41 @@
+# Import libraries
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from pathlib import Path
+
+def generateEncoding(): 
+    # Import dataset
+    df = pd.read_csv('../Labelling/original_labels.csv')
+    df = df[['file_name', 'fabric_group_name']]
+    df.rename(columns={'fabric_group_name': 'fabric'}, inplace=True)
+    # verify integrity
+    if df.file_name.nunique != df.file_name.nunique:
+        print("There are duplicated labels")
+
+    else:
+        df = df.dropna()  # remove rows with NaN values
+
+        # Label Encoding
+        df_labelEncoding = df.copy()
+        le = LabelEncoder()
+        df_labelEncoding.fabric = le.fit_transform(
+            df_labelEncoding.fabric.astype(str))
+        print(df_labelEncoding)
+        filepath = Path('../Labelling/labelEncoding.csv')
+        df_labelEncoding.to_csv(filepath)
+
+        le_name_mapping = pd.DataFrame(
+            zip(le.classes_, le.transform(le.classes_)), columns=['Class', 'EncodedLabel'])
+        print(le_name_mapping)
+        filepath = Path('../Labelling/labelEncodingMapping.csv')
+        le_name_mapping.to_csv(filepath)
+
+        # One-Hot Encoding
+        one_hot_encoded_data = pd.get_dummies(df, columns=['fabric'])
+        print(one_hot_encoded_data)
+        filepath = Path('../Labelling/oneHotEncoding.csv')
+        one_hot_encoded_data.to_csv(filepath)
+        
+if __name__ == '__main__':
+    generateEncoding()
--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ```
 conda create -n archaeoML
 conda activate archaeoML
-pip freeze > requirements.txt
+pip list --format=freeze > requirements.txt
 pip install -r requirements.txt
 ```
