8caa00b29ccc98a8ff9f51e93df270e629f9fb71
fiooonna <fiona10@hku.hk>
2023-01-22 16:40:23 +0800

    Split labels

--- Labelling/labelling.py
+++ Labelling/labelling.py
@@ -5,17 +5,17 @@ from pathlib import Path
 
 def generateEncoding(): 
     # Import dataset
-    df = pd.read_csv('../Labelling/original_labels.csv')
+    filepath = Path('../Labelling/original_labels.csv', index = False)
+    df = pd.read_csv(filepath)
     
     # Remove some typos 
-    df.fabric_group_name = df.fabric_group_name.str.replace(r'\s+', ' ', regex=True) # Remove double spaces
-    df.fabric_group_name = df.fabric_group_name.str.replace('rediish', 'reddish') # Remove double spaces
-    df.fabric_group_name = df.fabric_group_name.str.replace('liight', 'light') # Remove double spaces
-    df.fabric_group_name = df.fabric_group_name.str.replace('browm', 'brown') # Remove double spaces
-    df.fabric_group_name = df.fabric_group_name.str.replace('medum', 'medium') # Remove double spaces
-    df.fabric_group_name = df.fabric_group_name.str.lower()
-    filepath = Path('../Labelling/original_labels.csv', index = False)
-    df.to_csv(filepath)
+    # df.fabric_group_name = df.fabric_group_name.str.replace(r'\s+', ' ', regex=True) # Remove double spaces
+    # df.fabric_group_name = df.fabric_group_name.str.replace('rediish', 'reddish') # Remove double spaces
+    # df.fabric_group_name = df.fabric_group_name.str.replace('liight', 'light') # Remove double spaces
+    # df.fabric_group_name = df.fabric_group_name.str.replace('browm', 'brown') # Remove double spaces
+    # df.fabric_group_name = df.fabric_group_name.str.replace('medum', 'medium') # Remove double spaces
+    # df.fabric_group_name = df.fabric_group_name.str.lower()
+    # df.to_csv(filepath)
     
     df = df[['file_name', 'fabric_group_name']]
     df.rename(columns={'fabric_group_name': 'fabric'}, inplace=True)
@@ -25,9 +25,14 @@ def generateEncoding():
 
     else:
         df = df.dropna()  # remove rows with NaN values
-
+        # df['fabric'] = df.fabric.str.rstrip('-1234567890.') # Remove -01, -02, ... in each labels 
+        # df[['color','fabric']] = df.fabric.str.extract('^(.*?)\s((?:dark|light)?\s?\w+)$') # Remove -01, -02, ... in each labels 
+        # print(df.head())
         # Label Encoding
         df_labelEncoding = df.copy()
+        
+        
+        
         le = LabelEncoder()
         df_labelEncoding.fabric = le.fit_transform(
             df_labelEncoding.fabric.astype(str))
@@ -37,7 +42,6 @@ def generateEncoding():
         le_name_mapping = pd.DataFrame(
             zip(le.classes_, le.transform(le.classes_)), columns=['Class', 'EncodedLabel'])
         filepath = Path('../Labelling/labelEncodingMapping.csv')
-        print(le_name_mapping)
         le_name_mapping.to_csv(filepath)
 
         # One-Hot Encoding
