fdd63644bf769b92c45854002a5853389fd4b9f6
fiooonna <fiona10@hku.hk>
2023-03-04 13:41:36 +0800

    minor fix

--- Training/paraDict.py
+++ Training/paraDict.py
@@ -1,21 +1,9 @@
-# Basic set up
-import sys
-sys.path.append('../')
-
-import cv2
-import numpy as np
-import pandas as pd
-import configure as cfg
-
 
 import torch
 import torchvision
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
-from torch.utils.data import Dataset
-from torchvision import datasets, transforms, models
-from torchvision.transforms import ToTensor
 
 # ================= Parameters 1 ====================== 
 PARAS_1 = {
